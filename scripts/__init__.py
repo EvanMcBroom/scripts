4e6ce5f96a51018599c935c07b6745923ff68d28
Evan McBroom <15305789+EvanMcBroom@users.noreply.github.com>
2022-09-17 22:41:15 -0400

    Added import for bhquery

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.2'
+__version__ = '1.1'
 __author__ = 'Evan McBroom'
 
+from .bhquery import *
 from .https import *
 from .scan import *
 from .urlparse import *
